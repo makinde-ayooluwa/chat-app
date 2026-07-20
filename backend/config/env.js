82ce03bae927e5140203b5c7de7c332210a7c7c7
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-07-20 11:37:46 +0100

    chat app turned to book app

--- backend/config/env.js
+++
@@ -1,6 +0,0 @@
-const ENV = {
-    MONGODB_URL: "mongodb+srv://makindeayooluwa604_db_user:victorvictor@intro-to-backend.ac2iqpb.mongodb.net/",
-    SOCKET_PORT: 8080,
-    SERVER_PORT: 8000
-}
-module.exports = ENV;
---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const app = require("./app")
+const dotenv = require("dotenv")
+dotenv.config({
+    path:"./.env"
+})
+const startServer = ()=>{
+    const PORT = process.env.SERVER_PORT || 3000;
+    app.listen(PORT, ()=>{
+        console.log(`Server listening on port ${PORT}`)
+    })
+}
+startServer();
